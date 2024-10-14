087cd9525426e5941ae53f37fa4d3aafeb0f239a
Delavuida <jaju1995@naver.com>
2024-10-14 13:59:40 +0900

    Edit #14 댓글 관리 view
    바뀐 카멜케이스 컨트롤러에도 적용

--- src/main/java/com/fastcampus/board_admin_project/controller/ArticleCommentManagementController.java
+++ src/main/java/com/fastcampus/board_admin_project/controller/ArticleCommentManagementController.java
@@ -22,7 +22,7 @@ public class ArticleCommentManagementController {
             Model model
     ) {
         model.addAttribute("request", request);
-        return "management/articleComments";
+        return "management/article-comments";
     }
 
 }
